5fa247473b58eee3361b608534293075d5b3ebed
namesake08 <namesakeakzet@gmail.com>
2020-10-10 23:08:27 +0300

    adding a solution for JsonTask

--- ParseJsonTask/parse.py
+++ ParseJsonTask/parse.py
@@ -1,55 +1,73 @@
-# ----------------------------
-# Test task by Slava K.
-#
-# Date
-# 2/18/2020
-# ----------------------------
+#!/usr/bin/python
 import json
 
-filename = "ts_info.json"
-with open(filename) as TS_file_info:
-    try:
-        def recursive_iter(obj, keys=()):
-            if isinstance(obj, dict):
-                for k, v in obj.items():
-                    yield from recursive_iter(v, keys + (k,))
-            elif any(isinstance(obj, t) for t in (list, tuple)):
-                for idx, item in enumerate(obj):
-                    yield from recursive_iter(item, keys + (idx,))
-            else:
-                yield keys, obj
+FILENAME = "ts_info.json"
 
-        TS_file_json_data = json.load(TS_file_info)
-        for keys, item in recursive_iter(TS_file_json_data):
-            str_audio_bool = True if 'Audio' == keys else False
-            str_video_bool = True if 'Video' == keys else False
-            has_a_logo = True if str_video_bool == True else False
-            number_of_logos = 0
-            if has_a_logo:
-                number_of_logos += 1
 
-        # Audio condition
-        if str_audio_bool:
-            print('TS file has audio')
+def get_json():
+    with open(FILENAME) as TS_file_info:
+        ts_info = json.load(TS_file_info)
+        print(ts_info)
+        return ts_info
+
+
+def check_audio(ts_objects_):
+    for i in ts_objects_:
+        if i.get('Audio'):
+            return True
+
+
+def check_logos(ts_objects_):
+    for i in ts_objects_:
+        logos_ = i.get("Video", {}).get("Content", {}).get("Logos", {})
+        if logos_:
+            return logos_.get("LogoOnList")
+
+
+def get_coordinates(logos):
+    for i in logos:
+        print(i.get("X", {}))
+        x = i.get("X", {})
+        if x <= 860:
+            print('The log is on the left part of the screen')
         else:
-            print('TS file has no audio')
+            print('The log is on the right part of the screen')
 
-        # Number of logos condition
-        if has_a_logo:
-            print('TS file has {} logos'.format(number_of_logos))
-        else:
-            print('TS file has no logos in video')
 
-        # # Logo location condition
-        # x = dict_from_parsed_json[key]
-        # if x <= 860:
-        #     print('The log is on the left part of the screen')
-        # else:
-        #     print('The log is on the right part of the screen')
+def print_hex(ts_objects_):
+    for i in ts_objects_:
+        es_list = i.get("PMT", {}).get("ProgramInfo", {}).get("ElementStream", {})
+    es_pid = es_list[0].get("PID", {})
+    print('\\' + hex(es_pid))
 
-        # # Video PID in hex format
-        # PID = 32
-        # print('\\' + hex(PID)[1:])
 
-    finally:
-        TS_file_info.close()
+if __name__ == '__main__':
+    # ts_info = get_json()
+    ts_objects = get_json().get('TransportStream')
+    has_audio = check_audio(ts_objects)
+    if has_audio:
+        print("TS object has audio")
+    else:
+        print("TS object has no audio")
+
+    logos = check_logos(ts_objects)
+    if logos:
+        print(f"TS file has video with {len(logos)} logo")
+    else:
+        print("TS file has no video with logo")
+
+    get_coordinates(logos)
+    print_hex(ts_objects)
+
+
+
+
+
+    # knights = dict_with_dicts.get('TransportStream')
+    # print(dict_of_dicts.get("LogoOnList"))
+    # for k, v in knights.items(): #только со словаря
+    #     print(k, v)
+    #
+    #     need_value = json.get('first_key', {}).get('second_key', None)
+    #     if not need_value:
+    #         print('There is no key "need_value" in passed JSON')
